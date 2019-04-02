5975cd0c5f5f4dbf27f480f1d029405ab1ef1876
John Vandenberg <jayvdb@gmail.com>
2019-04-02 20:10:05 +0700

    Split test_os tests
    Also only run test_os tests on Windows

--- tests/test_os.py
+++ tests/test_os.py
@@ -9,27 +9,68 @@ import unittest
 
 
 class FSEncodingTests(unittest.TestCase):
-    def test_nop(self):
-        self.assertEqual(os.fsencode(b'abc\xff'), b'abc\xff')
+    def test_nop_unicode(self):
         self.assertEqual(os.fsdecode('abc\u0141'), 'abc\u0141')
 
-    def test_identity(self):
-        # assert fsdecode(fsencode(x)) == x
-        for fn in ('unicode\u0141', 'latin\xe9', 'ascii'):
-            try:
-                bytesfn = os.fsencode(fn)
-            except UnicodeEncodeError:
-                continue
+    def test_nop_bytes(self):
+        self.assertEqual(os.fsencode(b'abc\xff'), b'abc\xff')
 
-            # XXX backport: Ignore bug in future.utils.surrogateescape.replace_surrogate_encode()
-            # by treating the below NameError like the above UnicodeEncodeError.
-            #
-            # Bug: https://github.com/PythonCharmers/python-future/issues/256
-            # (This workaround can be removed once that is fixed.)
-            except NameError as e:  # pragma: no cover
-                if e.message == "global name 'exc' is not defined":
-                    continue
-                else:
-                    raise
+    def test_identity_unicode(self):
+        fn = 'unicode\u0141'
+        try:
+            bytesfn = os.fsencode(fn)
+        except UnicodeEncodeError:
+            return
 
-            self.assertEqual(os.fsdecode(bytesfn), fn)
+        # XXX backport: Ignore bug in future.utils.surrogateescape.replace_surrogate_encode()
+        # by treating the below NameError like the above UnicodeEncodeError.
+        #
+        # Bug: https://github.com/PythonCharmers/python-future/issues/256
+        # (This workaround can be removed once that is fixed.)
+        except NameError as e:  # pragma: no cover
+            if e.message == "global name 'exc' is not defined":
+                return
+            else:
+                raise
+
+        self.assertEqual(os.fsdecode(bytesfn), fn)
+
+    def test_identity_latin(self):
+        fn = 'latin\xe9'
+        try:
+            bytesfn = os.fsencode(fn)
+        except UnicodeEncodeError:
+            return
+
+        # XXX backport: Ignore bug in future.utils.surrogateescape.replace_surrogate_encode()
+        # by treating the below NameError like the above UnicodeEncodeError.
+        #
+        # Bug: https://github.com/PythonCharmers/python-future/issues/256
+        # (This workaround can be removed once that is fixed.)
+        except NameError as e:  # pragma: no cover
+            if e.message == "global name 'exc' is not defined":
+                return
+            else:
+                raise
+
+        self.assertEqual(os.fsdecode(bytesfn), fn)
+
+    def test_identity_ascii(self):
+        fn = 'ascii'
+        try:
+            bytesfn = os.fsencode(fn)
+        except UnicodeEncodeError:
+            return
+
+        # XXX backport: Ignore bug in future.utils.surrogateescape.replace_surrogate_encode()
+        # by treating the below NameError like the above UnicodeEncodeError.
+        #
+        # Bug: https://github.com/PythonCharmers/python-future/issues/256
+        # (This workaround can be removed once that is fixed.)
+        except NameError as e:  # pragma: no cover
+            if e.message == "global name 'exc' is not defined":
+                return
+            else:
+                raise
+
+        self.assertEqual(os.fsdecode(bytesfn), fn)
